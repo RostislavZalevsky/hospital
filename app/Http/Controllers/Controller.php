ba14190ae9e1dbdccd7a99a00884b6dbfc05ea1f
glorygrows <z.rostislav11@gmail.com>
2022-05-27 00:54:53 -0700

    upd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,15 +10,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /*
-     Encryption keys generated successfully.
-Personal access client created successfully.
-Client ID: 1
-Client secret: sZ3UAAByflGHSyyd4SBrjNMOKflHTRxUJ9eOjQVj
-Password grant client created successfully.
-Client ID: 2
-Client secret: QgwmXGGnmSH5LzwjTbizPQpLBvcRoGWn18Bcvpbw
-
-     */
 }
